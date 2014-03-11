d0cb3d25578426500e6b3ef9f3dfac66c6189f8f
faassen <faassen@startifact.com>
2014-03-11 16:47:25 +0100

    The app can get input now.

--- morepath_react/static/main.js
+++ morepath_react/static/main.js
@@ -14,7 +14,7 @@
                     this.setState(data);
                 }.bind(this));
             } else if (this.props.obj !== undefined) {
-                this.setState(obj);
+                this.setState(this.props.obj);
             }
         },
         render: function() {
@@ -27,47 +27,82 @@
         }
     });
 
+
+    // may want to have a render variety where we specify the
+    // iface (or component to use?) directly with the result from the URL
+
     var view = function(info) {
+        // can possibly get iface from callee, see:
+        // https://github.com/facebook/react/commit/f0fdabae7bbeadde9245d00893b194e0310c8d9b
+        // for how React does it
+        info.displayName = info.iface;
         var klass = React.createClass(info);
         registry[info.iface] = klass;
         return klass;
     };
 
-    // var DocumentCollection = view({
-    //     iface: 'DocumentCollection',
-    //     getInitialState: function() {
-    //         return { documents: [],
-    //                  previous: null,
-    //                  next: null,
-    //                  add: null };
-    //     },
-    //     componentWillMount: function() {
-    //         $.getJSON(this.props.url).done(function(data) {
-    //             this.setState(data);
-    //         }.bind(this));
-    //     },
-    //     render: function() {
-    //         var items = this.state.documents.map(function(document) {
-    //             return <li key={document.id}>{document.title}</li>;
-    //         });
-    //         return (
-    //                 <ul>
-    //                 {items}
-    //             </ul>
-    //         );
-    //     }
-    // });
+    var Document = view({
+        iface: 'Document',
+        render: function() {
+           return <li>{this.props.title}</li>;
+        }
+    });
+
+    var DocumentAdd = React.createClass({
+        getInitialState: function() {
+            return {title: '',
+                    content: ''};
+        },
+        handleTitleChange: function(event) {
+            this.setState({title: event.target.value});
+        },
+        handleContentChange: function(event) {
+            this.setState({content: event.target.value});
+        },
+        handleSubmit: function(event) {
+            this.props.handleSubmit(this.state);
+        },
+        render: function() {
+            return (
+              <form>
+                <input type="text" value={this.state.title}
+                  onChange={this.handleTitleChange} />
+                <input type="text" value={this.state.content}
+                  onChange={this.handleContentChange} />
+                <input type="button" value="Submit"
+                  onClick={this.handleSubmit} />
+            </form>);
+        }
+    });
 
     var DocumentCollection = view({
         iface: 'DocumentCollection',
+        getInitialState: function() {
+            return this.props;
+        },
+        handleSubmit: function(data) {
+            $.ajax({
+                url: this.state.add,
+                contentType: 'application/json',
+                dataType: 'json',
+                type: 'POST',
+                processData: false,
+                data: JSON.stringify(data)
+            }).done(function(data) {
+                this.setState(data);
+            }.bind(this));
+        },
         render: function() {
-            var items = this.props.documents.map(function(document) {
-                return <li key={document.id}>{document.title}</li>;
+            var items = this.state.documents.map(function(document) {
+                return <Render key={document.id} obj={document} />
             });
             return (
+                 <div>
                     <ul>
-                    {items}
-                </ul>
+                      {items}
+                    </ul>
+                    <DocumentAdd handleSubmit={this.handleSubmit} />
+                 </div>
             );
         }
     });
--- morepath_react/view.py
+++ morepath_react/view.py
@@ -29,10 +29,13 @@ def info(self, request):
 
 @app.json(model=Document)
 def document_default(self, request):
-    return {'id': self.id,
-            'title': self.title,
-            'content': self.content,
-            'link': request.link(self) }
+    return {
+        'iface': 'Document',
+        'id': self.id,
+        'title': self.title,
+        'content': self.content,
+        'link': request.link(self)
+    }
 
 
 @app.json(model=DocumentCollection)
@@ -46,24 +49,9 @@ def document_collection_default(self, request):
         }
 
 
-@app.html(model=DocumentCollection, name='add')
+@app.json(model=DocumentCollection, name='add', request_method='POST')
 def document_collection_add(self, request):
-    return '''\
-<html>
-<body>
-<form action="/documents/add_submit" method="POST">
-title: <input type="text" name="title"><br>
-content: <input type="text" name="content"><br>
-<input type="submit" value="Add!"><br>
-</form>
-</body>
-</html>
-'''
-
-
-@app.html(model=DocumentCollection, name='add_submit', request_method='POST')
-def document_collection_add_submit(self, request):
-    title = request.POST.get('title')
-    content = request.POST.get('content')
+    title = request.json.get('title')
+    content = request.json.get('content')
     document = self.add(title=title, content=content)
-    return "<p>Awesome %s</p>" % document.id
+    return request.view(self)
